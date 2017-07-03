a7c127c448efd015db41e3c49fc9624e8e3f3d7d
kris <krisshamloo@gmail.com>
2017-07-03 14:46:22 -0700

    rewrite block in Swift

---
+++ block.swift
@@ -0,0 +1,40 @@
+#!/usr/bin/swift
+import Foundation
+
+let emoji: [String] = [
+    "🐶",
+    "🦁",
+    "🐺",
+    "🐉",
+    "🐘",
+    "🌺",
+    "🍁",
+    "🍦",
+    "🍓",
+    "🍏",
+    "🚗",
+    "⛵",
+    "🦋",
+    "🐳",
+    "🌸",
+    "⭐️",
+    "🍎",
+    "🚲",
+    "⚔️",
+    "🇺🇸",
+]
+
+let d = Date()
+let choice = Int(arc4random()) % emoji.count
+let e = emoji[choice]
+let dateFormatter = DateFormatter()
+dateFormatter.dateStyle = .medium
+dateFormatter.timeStyle = .medium
+
+let description = dateFormatter.string(from: d)
+let len = description.count
+let bar = String.init(repeating: "━", count: len + 2)
+
+print("┏\(bar)┓")
+print("┃ \(description) ┃ \(e)")
+print("┗\(bar)┛")
