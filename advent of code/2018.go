b375fd69930a2ce6f97cc52ec5ff51a4e1f54d2f
Kris Shamloo <krisshamloo@gmail.com>
2018-12-01 08:01:54 -0800

    day one

---
+++ advent of code/2018.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("It's time to save Christmas. 🎄")
+
+	fmt.Println(dayOneA(dayOneInput))
+	fmt.Println(dayOneB(dayOneInput))
+}
+
+func dayOneA(input string) int {
+	freq := 0
+
+	for _, val := range strings.Split(input, ", ") {
+		v, err := strconv.Atoi(val)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		freq += v
+	}
+
+	return freq
+}
+
+func dayOneB(input string) int {
+	freq := 0
+	seen := make(map[int]bool)
+
+	for {
+		for _, val := range strings.Split(input, ", ") {
+			v, err := strconv.Atoi(val)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			freq += v
+
+			alreadySeen, _ := seen[freq]
+			if alreadySeen {
+				return freq
+			} else {
+				seen[freq] = true
+			}
+		}
+	}
+
+	return freq
+}
