041b686a42776afd622c908e0f4bd52a9eb56923
Kris Shamloo <krisshamloo@gmail.com>
2018-12-02 07:51:46 -0800

    day two

--- advent of code/2018.go
+++ advent of code/2018.go
@@ -9,8 +9,88 @@ import (
 func main() {
 	fmt.Println("It's time to save Christmas. 🎄")
 
-	fmt.Println(dayOneA(dayOneInput))
-	fmt.Println(dayOneB(dayOneInput))
+	// fmt.Println(dayOneA(dayOneInput))
+	// fmt.Println(dayOneB(dayOneInput))
+
+	fmt.Println(dayTwoA(dayTwoInput))
+	fmt.Println(dayTwoB(dayTwoInput))
+}
+
+func dayTwoA(input string) int {
+	twos := 0
+	threes := 0
+
+	for _, val := range strings.Split(input, ",") {
+		counts := make(map[rune]int)
+
+		for _, r := range val {
+			counts[r] = 0
+		}
+		for _, r := range val {
+			counts[r] += 1
+		}
+
+		incTwos := false
+		incThrees := false
+		for _, v := range counts {
+			if v == 2 {
+				incTwos = true
+			}
+			if v == 3 {
+				incThrees = true
+			}
+		}
+
+		if incTwos {
+			twos++
+		}
+		if incThrees {
+			threes++
+		}
+	}
+
+	return twos * threes
+}
+
+func dayTwoB(input string) string {
+	inputs := strings.Split(input, ",")
+	var matches [2]string
+
+	for i, id1 := range inputs {
+		for j, id2 := range inputs {
+			if i == j {
+				continue
+			}
+
+			variance := 0
+
+			for k, _ := range id1 {
+				if id1[k] != id2[k] {
+					variance++
+
+					if variance > 1 {
+						break
+					}
+				}
+			}
+
+			if variance == 1 {
+				matches[0] = id1
+				matches[1] = id2
+			} else {
+				continue
+			}
+
+		}
+	}
+
+	match := ""
+	for i, _ := range matches[0] {
+		if matches[0][i] == matches[1][i] {
+			match = match + string(matches[0][i])
+		}
+	}
+	return match
 }
 
 func dayOneA(input string) int {
