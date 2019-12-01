d0109f2e6da4fa9e45f711872aec3c7fdd4d5bfa
Kris Shamloo <kris-s@users.noreply.github.com>
2019-12-01 09:29:17 -0500

    2019 day 1

--- advent-of-code/2019/day01.go
+++ advent-of-code/2019/day01.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Let's save Christmas!")
+	lines := []string{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	nums := convert(lines)
+	fmt.Println("Day one:", sumRocket(nums, rocket1))
+	fmt.Println("Day two:", sumRocket(nums, rocket2))
+}
+
+func convert(lines []string) []int {
+	nums := []int{}
+	for _, l := range lines {
+		value, err := strconv.Atoi(l)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums = append(nums, value)
+	}
+	return nums
+}
+
+func sumRocket(nums []int, eq func(int) int) int {
+	count := 0
+	for i := range nums {
+		count += eq(nums[i])
+	}
+	return count
+}
+
+func rocket1(value int) int {
+	value /= 3
+	value -= 2
+	return value
+}
+
+func rocket2(value int) int {
+	total := rocket1(value)
+	temp := total
+	for temp > 0 {
+		temp = rocket1(temp)
+		if temp > 0 {
+			total += temp
+		}
+	}
+	return total
 }
