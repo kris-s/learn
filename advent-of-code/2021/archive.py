db95152b5113a3ecfa67440ff3e89d13b13d94bf
Kris Shamloo <krisshamloo@gmail.com>
2021-12-08 21:35:50 -0800

    Day 9

--- advent-of-code/2021/archive.py
+++ advent-of-code/2021/archive.py
@@ -9,6 +9,169 @@ def read_file(filename):
 
 
 
+# --- day eight ---
+
+
+"""
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+"""
+
+SEGMENT_FREQ = {
+    "a": 8,
+    "b": 6,
+    "c": 8,
+    "d": 7,
+    "e": 4,
+    "f": 9,
+    "g": 7,
+}
+
+
+IDEAL_WIRING = {
+    0: "abcefg",
+    1: "cf",
+    2: "acdeg",
+    3: "acdfg",
+    4: "bcdf",
+    5: "abdfg",
+    6: "abdefg",
+    7: "acf",
+    8: "abcdefg",
+    9: "abcdfg",
+}
+
+
+
+
+SEGMENT_LENGTHS = {
+    0: 6,
+    1: 2,
+    2: 5,
+    3: 5,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 3,
+    8: 7,
+    9: 6,
+}
+
+
+def day_eight_prep(data):
+    output = []
+    for row in data.split("\n"):
+        if not row:
+            continue
+        left, right = row.split("|")
+        left_segments = left.split()
+        right_segments = right.split()
+        output.append((left_segments, right_segments))
+
+    return output
+
+
+def day_eight_a(data):
+    data = day_eight_prep(data)
+    count = 0
+
+    for _, output_values in data:
+        # 1, 4, 7, and 8
+        lengths = [len(v) for v in output_values]
+        count += lengths.count(SEGMENT_LENGTHS[1])
+        count += lengths.count(SEGMENT_LENGTHS[4])
+        count += lengths.count(SEGMENT_LENGTHS[7])
+        count += lengths.count(SEGMENT_LENGTHS[8])
+
+    return count
+
+
+def get_wiring_map(signals):
+    counts = Counter("".join(signals))
+    segment_map = {}
+
+    for ideal_wire, count in SEGMENT_FREQ.items():
+        if count in (7, 8):
+            continue
+
+        for real_wire, real_count in counts.items():
+            if real_count == count:
+                segment_map[ideal_wire] = real_wire
+
+    segments = {"a", "b", "c", "d", "e", "f", "g"}
+    lengths_map = {len(sig): set(sig) for sig in signals}
+
+    one = lengths_map[SEGMENT_LENGTHS[1]]
+    four = lengths_map[SEGMENT_LENGTHS[4]]
+    seven = lengths_map[SEGMENT_LENGTHS[7]]
+
+    # in seven but not in one
+    segment_map["a"] = (seven ^ one).pop()
+
+    # the 8 count in one
+    for wire in one:
+        if wire not in segment_map.values():
+            segment_map["c"] = wire
+
+    # d from four
+    for wire in four:
+        if wire not in segment_map.values():
+            segment_map["d"] = wire
+
+    # g remains
+    segment_map["g"] = (segments ^ set(segment_map.values())).pop()
+
+    wiring_map = {}
+    for number, ideal in IDEAL_WIRING.items():
+        key = ""
+        for wire in ideal:
+            key += segment_map[wire]
+
+        key = "".join(sorted(key))
+        wiring_map[key] = str(number)
+
+    return wiring_map
+
+
+def day_eight_b(data):
+    data = day_eight_prep(data)
+
+    total = 0
+
+    for signals, output_values in data:
+        wiring_map = get_wiring_map(signals)
+        display = ""
+
+        for digit in output_values:
+            display += wiring_map["".join(sorted(digit))]
+
+        total += int(display)
+
+    return total
+
+
+# day_eight_input = read_file("day08.txt")
+# print(day_eight_a(example))
+# print(day_eight_a(day_eight_input))
+# print(day_eight_b(example))
+# print(day_eight_b(day_eight_input))
+
+
 # -- day seven --
 
 
--- advent-of-code/2021/submarine.py
+++ advent-of-code/2021/submarine.py
@@ -5,172 +5,117 @@ from archive import read_file
 
 print("Saving Christmas again!")
 
-
-"""
-  0:      1:      2:      3:      4:
- aaaa    ....    aaaa    aaaa    ....
-b    c  .    c  .    c  .    c  b    c
-b    c  .    c  .    c  .    c  b    c
- ....    ....    dddd    dddd    dddd
-e    f  .    f  e    .  .    f  .    f
-e    f  .    f  e    .  .    f  .    f
- gggg    ....    gggg    gggg    ....
-
-  5:      6:      7:      8:      9:
- aaaa    aaaa    aaaa    aaaa    aaaa
-b    .  b    .  .    c  b    c  b    c
-b    .  b    .  .    c  b    c  b    c
- dddd    dddd    ....    dddd    dddd
-.    f  e    f  .    f  e    f  .    f
-.    f  e    f  .    f  e    f  .    f
- gggg    gggg    ....    gggg    gggg
-"""
-
-SEGMENT_FREQ = {
-    "a": 8,
-    "b": 6,
-    "c": 8,
-    "d": 7,
-    "e": 4,
-    "f": 9,
-    "g": 7,
-}
-
-
-IDEAL_WIRING = {
-    0: "abcefg",
-    1: "cf",
-    2: "acdeg",
-    3: "acdfg",
-    4: "bcdf",
-    5: "abdfg",
-    6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
-    9: "abcdfg",
-}
-
-example = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+example = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
 """
 
 
-SEGMENT_LENGTHS = {
-    0: 6,
-    1: 2,
-    2: 5,
-    3: 5,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 3,
-    8: 7,
-    9: 6,
-}
-
-
-def day_eight_prep(data):
-    output = []
-    for row in data.split("\n"):
+def day_nine_prep(data):
+    grid = []
+    for row in data.split('\n'):
         if not row:
             continue
-        left, right = row.split("|")
-        left_segments = left.split()
-        right_segments = right.split()
-        output.append((left_segments, right_segments))
-
-    return output
+        grid.append([int(v) for v in row])
+    return grid
 
 
-def day_eight_a(data):
-    data = day_eight_prep(data)
-    count = 0
+def grid_adjacents(grid, x, y):
+    up = None
+    down = None
+    left = None
+    right = None
 
-    for _, output_values in data:
-        # 1, 4, 7, and 8
-        lengths = [len(v) for v in output_values]
-        count += lengths.count(SEGMENT_LENGTHS[1])
-        count += lengths.count(SEGMENT_LENGTHS[4])
-        count += lengths.count(SEGMENT_LENGTHS[7])
-        count += lengths.count(SEGMENT_LENGTHS[8])
+    if y > 0:
+        up = grid[y-1][x]
 
-    return count
+    if y < len(grid) - 1:
+        down = grid[y+1][x]
 
+    if x > 0:
+        left = grid[y][x-1]
 
-def get_wiring_map(signals):
-    counts = Counter("".join(signals))
-    segment_map = {}
+    if x < len(grid[0]) - 1:
+        right = grid[y][x+1]
 
-    for ideal_wire, count in SEGMENT_FREQ.items():
-        if count in (7, 8):
-            continue
-
-        for real_wire, real_count in counts.items():
-            if real_count == count:
-                segment_map[ideal_wire] = real_wire
-
-    segments = {"a", "b", "c", "d", "e", "f", "g"}
-    lengths_map = {len(sig): set(sig) for sig in signals}
-
-    one = lengths_map[SEGMENT_LENGTHS[1]]
-    four = lengths_map[SEGMENT_LENGTHS[4]]
-    seven = lengths_map[SEGMENT_LENGTHS[7]]
-
-    # in seven but not in one
-    segment_map["a"] = (seven ^ one).pop()
-
-    # the 8 count in one
-    for wire in one:
-        if wire not in segment_map.values():
-            segment_map["c"] = wire
-
-    # d from four
-    for wire in four:
-        if wire not in segment_map.values():
-            segment_map["d"] = wire
-
-    # g remains
-    segment_map["g"] = (segments ^ set(segment_map.values())).pop()
-
-    wiring_map = {}
-    for number, ideal in IDEAL_WIRING.items():
-        key = ""
-        for wire in ideal:
-            key += segment_map[wire]
-
-        key = "".join(sorted(key))
-        wiring_map[key] = str(number)
-
-    return wiring_map
+    return [v for v in [up, down, left, right] if v is not None]
 
 
-def day_eight_b(data):
-    data = day_eight_prep(data)
+def grid_adjacents_points(grid, x, y):
+    up = None
+    down = None
+    left = None
+    right = None
 
-    total = 0
+    if y > 0:
+        up = (x, y-1, grid[y-1][x])
 
-    for signals, output_values in data:
-        wiring_map = get_wiring_map(signals)
-        display = ""
+    if y < len(grid) - 1:
+        down = (x, y+1, grid[y+1][x])
 
-        for digit in output_values:
-            display += wiring_map["".join(sorted(digit))]
+    if x > 0:
+        left = (x-1, y, grid[y][x-1])
 
-        total += int(display)
+    if x < len(grid[0]) - 1:
+        right = (x+1, y, grid[y][x+1])
 
-    return total
+    return [v for v in [up, down, left, right] if v is not None]
 
 
-# day_eight_input = read_file("day08.txt")
-# print(day_eight_a(example))
-# print(day_eight_a(day_eight_input))
-# print(day_eight_b(example))
-# print(day_eight_b(day_eight_input))
+def grid_basin(grid, x, y):
+    basin_points = set()
+
+    def walk(grid, x, y, basin_points):
+        for x, y, value in grid_adjacents_points(grid, x, y):
+            if (x, y) not in basin_points and value != 9:
+                basin_points.add((x, y))
+                walk(grid, x, y, basin_points)
+
+    walk(grid, x, y, basin_points)
+    return basin_points
+
+
+def day_nine_a(data):
+    grid = day_nine_prep(data)
+    print(grid)
+
+    low_points = []
+
+    for y, row in enumerate(grid):
+        for x, value in enumerate(row):
+            is_low_point = True
+            for neighbor in grid_adjacents(grid, x, y):
+                if neighbor <= value:
+                    is_low_point = False
+
+            if is_low_point:
+                low_points.append((x, y, value+1))
+
+    return sum(p[2] for p in low_points)
+
+
+def day_nine_b(data):
+    grid = day_nine_prep(data)
+    seen_basin_points = set()
+    basins = []
+
+    for y, row in enumerate(grid):
+        for x, value in enumerate(row):
+            if value != 9 and (x,y) not in seen_basin_points:
+                basin_points = grid_basin(grid, x, y)
+                for point in basin_points:
+                    seen_basin_points.add(point)
+
+                basins.append(len(basin_points))
+
+    basins = sorted(basins, reverse=True)
+
+    return basins[0] * basins[1] * basins[2]
+
+
+# day_nine_input = read_file('day09.txt')
+# print(day_nine_a(day_nine_input))
+# print(day_nine_b(day_nine_input))
