add141f2326e65490765d9515dfb1ceb9beb7139
Kris Shamloo <krisshamloo@gmail.com>
2023-12-01 21:23:24 -0800

    aoc23 day 2

--- advent-of-code/2023/days.swift
+++ advent-of-code/2023/days.swift
@@ -18,3 +18,65 @@ func readLines(filename: String) -> [Substring] {
     
     return content.split(separator: "\n")
 }
+
+func day1() {
+    let digits: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    let digitsSpelled: [String:String] = [
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    ]
+    var calibration = 0
+
+    for line in readLines(filename: "/Users/kris/Documents/aoc23/1.txt") {
+        if line.isEmpty {
+            continue
+        }
+
+        var first = ""
+        var last = ""
+        var scan = ""
+
+        for ch in line {
+            guard first.isEmpty else { break }
+            scan += String(ch)
+            if digits.contains(ch) {
+                first = String(ch)
+                break
+            }
+            for key in digitsSpelled.keys {
+                if scan.contains(key) {
+                    first = digitsSpelled[key]!
+                    break
+                }
+            }
+        }
+
+        scan = ""
+        for ch in line.reversed() {
+            guard last.isEmpty else { break }
+
+            scan = "\(ch)\(scan)"
+            if digits.contains(ch) {
+                last = String(ch)
+            }
+            for key in digitsSpelled.keys {
+                if scan.contains(key) {
+                    last = digitsSpelled[key]!
+                }
+            }
+        }
+
+        let value = Int("\(first)\(last)") ?? 0
+        print("\(line) adding", "\(value)")
+        calibration += value
+        print("----")
+    }
+    print(calibration)
+}
--- advent-of-code/2023/main.swift
+++ advent-of-code/2023/main.swift
@@ -4,70 +4,44 @@
 
 import Foundation
 
-let EXAMPLE = "/Users/kris/Documents/aoc23/input.example"
-let DAY1 = "/Users/kris/Documents/aoc23/1.txt"
+let EXAMPLE = "/Users/kris/Documents/aoc23/example.txt"
 
+func day2() {
+    let path = "/Users/kris/Documents/aoc23/2.txt"
 
-func day1() {
-    let digits: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    let digitsSpelled: [String:String] = [
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    ]
-    var calibration = 0
+    var total = 0
 
-    for line in readLines(filename: DAY1) {
+    for line in readLines(filename: path) {
         if line.isEmpty {
             continue
         }
-        
-        var first = ""
-        var last = ""
-        var scan = ""
-        
-        for ch in line {
-            guard first.isEmpty else { break }
-            scan += String(ch)
-            if digits.contains(ch) {
-                first = String(ch)
-                break
-            }
-            for key in digitsSpelled.keys {
-                if scan.contains(key) {
-                    first = digitsSpelled[key]!
-                    break
-                }
-            }
-        }
-        
-        scan = ""
-        for ch in line.reversed() {
-            guard last.isEmpty else { break }
-            
-            scan = "\(ch)\(scan)"
-            if digits.contains(ch) {
-                last = String(ch)
-            }
-            for key in digitsSpelled.keys {
-                if scan.contains(key) {
-                    last = digitsSpelled[key]!
+
+        var table: [String: Int] = [
+            "r": 0,
+            "g": 0,
+            "b": 0,
+        ]
+
+        let components = line.split(separator: " ")
+
+        for (i, c) in components.enumerated() {
+            if let value = Int(c) {
+                if let maxSeen = table[String(components[i+1].first!)] {
+                    if value > maxSeen {
+                        table[String(components[i+1].first!)] = value
+                    }
                 }
             }
         }
 
-        let value = Int("\(first)\(last)") ?? 0
-        print("\(line) adding", "\(value)")
-        calibration += value
-        print("----")
+        var power = 1
+        for v in table.values {
+            power *= v
+        }
+        total += power
+
     }
-    print(calibration)
+    print(total)
 }
 
-day1()
+day2()
